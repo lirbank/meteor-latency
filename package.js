c3122b344c6c0b23c7885f3be809a93ae1bdaa59
Mikael Lirbank <mikael@lirbank.com>
2015-02-16 21:23:33 -0800

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mikael:latency',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Simulate latency in Meteor apps',
   git: 'https://github.com/lirbank/meteor-latency.git',
   documentation: 'README.md'
